f829d43d9dc43bcb8cda121efdcdb3dec415d5c6
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-09-10 07:05:03 +0530

    Routing added and pages created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
-  return <div className='text-3xl font-bold underline'>I am Back</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/forgot-Password' element={<ForgotPassword />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
