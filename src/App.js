f721ca74fd18184913da4e397ce73474fbd6a595
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-09-10 06:30:40 +0530

    I am Back in this projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <div className='text-3xl font-bold underline'>Hello world</div>;
+  return <div className='text-3xl font-bold underline'>I am Back</div>;
 }
 
 export default App;
