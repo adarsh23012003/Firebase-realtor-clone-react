f6b0679ab4cedbcaf95c16aee545ea711e036417
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-09-11 17:35:37 +0530

    some css changes

--- src/Components/UI/LoadingIcon.js
+++ src/Components/UI/LoadingIcon.js
@@ -4,7 +4,7 @@ import loading from "../../assets/Svg/loading-svg.webp";
 function LoadingIcon() {
   return (
     <>
-      <div className='bg-black/10 bg-opacity-10 fixed z-30 flex h-screen w-full justify-center items-center'>
+      <div className='fixed inset-0 bg-black/10 bg-opacity-10 z-30 flex h-screen w-full justify-center items-center'>
         <img src={loading} alt='' className='h-24' />
       </div>
     </>
